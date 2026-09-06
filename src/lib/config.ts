10527daa07bb69079ff59495710746cc8aa72225
Earl Lee <earlvlee@gmail.com>
2026-09-06 15:53:52 -0400

    Publish the support address

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const APP_STORE_URL = '';
 
 export const SITE_NAME = 'Replay Rally';
-export const SUPPORT_EMAIL = '';
+export const SUPPORT_EMAIL = 'hello@replayrally.com';
 export const SUPPORT_EMPTY_STATE =
   'A support address will be posted here when Replay Rally ships on the App Store.';
